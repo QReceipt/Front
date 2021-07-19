14aae413b2281b91b6bc9a35adf20e6eb2dbcd48
C12H22O12 <director20844@gmail.com>
2021-07-20 00:34:14 +0900

    styled-component -> css

--- src/components/Presentational/UI/molecules/Login.js
+++ src/components/Presentational/UI/molecules/Login.js
@@ -2,21 +2,14 @@ import Btn from "../atoms/Btn.js"
 import Input from "../atoms/Input.js"
 import React from "react";
 import Title from "../atoms/Title.js"
-import styled from 'styled-components';
-
-const Receipt = styled.div`
-    display:inline;
-    float:left;
-`;
-
 
 const Login = ()=>{
     return (
         <div>
             <Title>로고로고로고</Title>
-            <Input></Input>
-            <Input></Input>
-            <Btn></Btn> <Btn></Btn> <Btn></Btn> 
+            <Input/>
+            <Input/>
+            <Btn/> <Btn/> <Btn/> 
         </div>
     )
 }
--- src/components/Presentational/UI/molecules/Receiptnum.js
+++ src/components/Presentational/UI/molecules/Receiptnum.js
@@ -2,19 +2,14 @@ import Btn from "../atoms/Btn.js"
 import Input from "../atoms/Input.js"
 import React from "react";
 import Title from "../atoms/Title.js"
-import styled from 'styled-componeents';
-
-const Receipt = styled.div`
-    display:block;
-`;
 
 const Receiptnum = ()=>{
     return (
-        <Receipt>
+        <div id="Receipt">
             <Title>로고로고로고</Title>
             <Input></Input>
             <Btn></Btn> 
-        </Receipt>
+        </div>
     )
 }
 
