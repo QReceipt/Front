2cc2ddab9cb104712df957b51be82277e02390d2
C12H22O12 <director20844@gmail.com>
2021-08-04 17:03:50 +0900

    21/08/04 router 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
-import Login from "./components/Presentational/pages/Register"
+import Reg from "./components/Presentational/pages/Register"
+import Login from "./components/Presentational/pages/Login"
+import SeeAll from "./components/Presentational/pages/SeeAll"
+import Map from "./components/Presentational/pages/Map"
+
+import {Route} from 'react-router-dom'
 
 function App() {
-    return (<div>
-        <Login/>
-        </div>);
+    return (
+    <div>
+        <Route path="/register" component={Reg}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/all" component={SeeAll} exact/>
+        <Route path="/map" component={Map}/>
+    </div>
+    );
 }
 
 export default App;
--- src/components/Presentational/UI/molecules/Nav.js
+++ src/components/Presentational/UI/molecules/Nav.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import Li from "../atoms/Li"
 
+import { Link } from "react-router-dom";
+
 const Nav = () => {
 
     return (
         <div id="Menu">
             <Li content="QReceipt 가맹점"/>
-            <Li content="영수증 상세보기"/>
-            <Li content="로그인"/>
+            <Link to="/register"><Li content="회원가입"/></Link>
+            <Link to="/login"><Li content="로그인"/></Link>
         </div>
     )
 }
