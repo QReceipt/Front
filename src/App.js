09fdc90cd5080e1bc27249fa86f179efae2cf740
C12H22O12 <director20844@gmail.com>
2021-08-11 16:41:14 +0900

    21/08/11 Seller 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Reg from "./components/Presentational/pages/Register"
 import Login from "./components/Presentational/pages/Login"
 import SeeAll from "./components/Presentational/pages/SeeAll"
 import Map from "./components/Presentational/pages/Map"
+import Seller from "./components/Presentational/pages/Seller"
 
 import {Route} from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" component={Login}/>
         <Route path="/all" component={SeeAll} exact/>
         <Route path="/map" component={Map}/>
+        <Route path="/Seller" component={Seller} />
     </div>
     );
 }
---
+++ src/components/Presentational/pages/Seller.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Date from "../UI/organisms/Date"
+import Receipt from "../UI/organisms/Receipt"
+import Nav from "../UI/organisms/Nav"
+import Page from "../UI/atoms/Page"
+import Footer from "../UI/atoms/Footer"
+import Detail from "../UI/organisms/Detail"
+import Table from "../UI/organisms/AllTable"
+import Title from "../UI/atoms/Title";
+import Controll from "../UI/molecules/ConPage"
+
+const SeeAll = () => {
+    return (
+        <div className="wrapper">
+            <div className="container-fluid">
+                <Nav/>
+                <div id="container">
+                    <div className="item">
+                        <div className="fstCont">
+                            <Title title="영수증 모아보기"/>
+                        </div>
+                        <Date/>
+                        <div id="all-table">
+                            <Table/>
+                        </div>
+                        <Page num="001"/>
+                    </div>
+                    <div className="item">
+                        <Receipt map="false"/>
+                        <Controll/>
+                        </div>
+                    <div className="item">
+                        <Detail/>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default SeeAll;
